10100e3c6174acce701e27871786aebc36b39747
ben.sefton@dpi.nsw.gov.au <ben.sefton@dpi.nsw.gov.au>
2020-03-13 11:21:42 +1100

    Removed EUI's

--- CSU/CSU-adt-lpp/LoRa-otaa.h
+++ CSU/CSU-adt-lpp/LoRa-otaa.h
@@ -1,10 +1,8 @@
-// app = dpi-test
-// ensure DEVEUI and APPEUI are both LSB
-static const u1_t PROGMEM DEVEUI[8]={ 0x27, 0x9A, 0xE4, 0xF3, 0x55, 0xCE, 0xA4, 0x00 };
-static const u1_t PROGMEM APPEUI[8]={ 0xC2, 0xC0, 0x02, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
-static const u1_t PROGMEM APPKEY[16] = { 0xB0, 0x3E, 0x60, 0x7F, 0x13, 0xD2, 0xEB, 0x6B, 0x4B, 0x06, 0xE3, 0x3C, 0xEC, 0xF1, 0x39, 0x1C };
 
-
+// ensure DEVEUI and APPEUI are both LSB with APPKEY being MSB
+static const u1_t PROGMEM DEVEUI[8]={ FILL_ME_IN };
+static const u1_t PROGMEM APPEUI[8]={ FILL_ME_IN };
+static const u1_t PROGMEM APPKEY[16] = { FILL_ME_IN };
 
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
--- CSU/CSU-lmic/LoRa-otaa.h
+++ CSU/CSU-lmic/LoRa-otaa.h
@@ -1,10 +1,8 @@
-// app = dpi-test
-// ensure DEVEUI and APPEUI are both LSB
-static const u1_t PROGMEM DEVEUI[8]={ 0x32, 0xB9, 0xF5, 0x0E, 0x09, 0x75, 0x40, 0x00 };
-static const u1_t PROGMEM APPEUI[8]={ 0x7B, 0xBE, 0x02, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
-static const u1_t PROGMEM APPKEY[16] = { 0xD1, 0x6D, 0x04, 0x3A, 0x5D, 0x6D, 0xED, 0x33, 0x00, 0xF5, 0x8B, 0x66, 0x8D, 0x4B, 0x3F, 0xDE };
 
-
+// ensure DEVEUI and APPEUI are both LSB with APPKEY being MSB
+static const u1_t PROGMEM DEVEUI[8]={ FILL_ME_IN };
+static const u1_t PROGMEM APPEUI[8]={ FILL_ME_IN };
+static const u1_t PROGMEM APPKEY[16] = { FILL_ME_IN };
 
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
